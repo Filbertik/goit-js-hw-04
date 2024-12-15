896b5036c900f905def1ac6830b56aae7b1d425e
Filbertik <aton2019zvit@gmail.com>
2024-12-15 02:00:22 +0200

    add tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,16 @@
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = pricePerDroid * quantity;
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function isEnoughCapacity(products, containerSize) {
+  const allItems = Object.values(products).reduce(
+    (total, count) => total + count,
+    0
+  );
+  return allItems <= containerSize;
 }
 
 // Виклик функції
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,25 @@
-function getElementWidth(content, padding, border) {
-  const contentValue = parseFloat(content);
-  const borderDouble = 2 * parseFloat(border);
-  const paddingDouble = 2 * parseFloat(padding);
-  const totalData = contentValue + paddingDouble + borderDouble;
-  return totalData;
-}
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 
 // Виклик функції
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
